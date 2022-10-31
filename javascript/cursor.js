2bfc7e71dda53276a410acbe88ee5e752c78ec10
Meganide <renashassan@hotmail.com>
2022-10-31 17:13:00 +0100

    changed cursor to moon

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -1,6 +1,5 @@
 export function cursor() {
   const cursor = document.querySelector('.cursor');
-  const cursorInner = document.querySelector('.cursor__inner');
   const cursorBorder = document.querySelector('.cursor__border');
   const anchors = document.querySelectorAll('a');
 
@@ -18,15 +17,15 @@ export function cursor() {
 
   anchors.forEach((anchor) => {
     anchor.addEventListener('mouseover', () => {
-      cursorInner.style.display = 'none';
       cursorBorder.style.display = 'none';
+      cursor.style.backgroundImage = 'none';
       cursor.style.transform = 'scale(2)';
       cursor.style.animationName = 'borderAnim';
       cursor.style.mixBlendMode = 'difference';
     });
     anchor.addEventListener('mouseleave', () => {
-      cursorInner.style.display = '';
       cursorBorder.style.display = 'block';
+      cursor.style.backgroundImage = 'url(../assets/images/cursor.png)';
       cursor.style.transform = '';
       cursor.style.animationName = '';
       cursor.style.mixBlendMode = '';
